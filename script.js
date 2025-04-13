83a2d8f31c1031eb66f9fbd3dc696f5579a5d0c2
chewycinn <hzlnmacaraeg@gmail.com>
2025-04-13 18:27:18 +0800

    main

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = "Hey love, I made this little website just for you. Every word here is from the heart, and every second of this audio is me thinking about you. ❤️";
+const message = "Hello my love 💕,\n\nThe day has come! It’s finally over — after everything you’ve gone through. Every drop of sweat and every tear, every pain and struggle... it’s all paid off. That’s why I made this little gift — to congratulate you, baby! For surviving this far. For not giving up, even when everything felt out of place. All the stress has finally eased.\n\nCongratulations — for giving your best, even when you were struggling with yourself. You deserve this, baby. You deserve every good thing that will come your way. I’m so, so, *so* proud of you for making it this far. Your past self would be in awe of where you are now — standing strong for yourself, your friends, your parents, and for God.\n\nI love you so much, baby. And I hope this small gift touches your heart and reminds you just how far you’ve come. ❤️";
 const textElement = document.getElementById("message");
 let i = 0;
 
